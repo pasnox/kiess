79af0f7492a90f28080ce64e9b41819b9f90f904
pasnox <pasnox@a98a87ac-f22f-11dd-8c4e-4bbb75a408b7>
2009-02-12 23:09:14 +0000

    Really add kSingleWidget...

---
+++ src/gui/kSingleWidget.h
@@ -0,0 +1,19 @@
+#ifndef KSINGLEWIDGET_H
+#define KSINGLEWIDGET_H
+
+#include "ui_kSingleWidget.h"
+
+class kSingleWidget : public kEmbeddedWidget, public Ui::kSingleWidget
+{
+	Q_OBJECT
+
+public:
+	kSingleWidget();
+	virtual ~kSingleWidget();
+
+public slots:
+	virtual void reject();
+	virtual void accept();
+};
+
+#endif // KSINGLEWIDGET_H
