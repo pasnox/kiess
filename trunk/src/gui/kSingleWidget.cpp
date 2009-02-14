d366404285257cf2c63ff8717478bf5a62d689b1
pasnox <pasnox@a98a87ac-f22f-11dd-8c4e-4bbb75a408b7>
2009-02-14 02:41:43 +0000

    Add mouse support for gui

--- trunk/src/gui/kSingleWidget.cpp
+++ trunk/src/gui/kSingleWidget.cpp
@@ -12,21 +12,17 @@ kSingleWidget::~kSingleWidget()
 
 void kSingleWidget::loadDatas()
 {
-	qWarning( "kSingleWidget::loadDatas()" );
 }
 
 void kSingleWidget::saveDatas()
 {
-	qWarning( "kSingleWidget::saveDatas()" );
 }
 
 void kSingleWidget::reject()
 {
-	qWarning( "kSingleWidget::reject()" );
 }
 
 void kSingleWidget::accept()
 {
 	saveDatas();
-	qWarning( "kSingleWidget::accept()" );
 }
