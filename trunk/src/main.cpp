9e680868e70114fb773be8a82c485b6890b57401
pasnox <pasnox@a98a87ac-f22f-11dd-8c4e-4bbb75a408b7>
2009-02-19 00:20:06 +0000

    Remove unneeded files

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,37 +1,30 @@
 #include <QApplication>
 #include <QDesktopWidget>
 
-#include "gui/kPanel.h"
+#include "gui/v2/kGui.h"
 #include "board/kBoard.h"
 #include "network/kiessxmpp.h"
-
-#include "gui/v2/kGui.h"
 
 int main(int argc, char ** argv)
 {
 	QApplication qa( argc, argv );
 	
-	/*
 	// create game panel
-	kPanel panel;
-	panel.setWindowTitle( "Kiess v0 - Kiess Team" );
+	kGui gui;
+	gui.setWindowTitle( "Kiess v0 - Kiess Team" );
 	
 	// center window
-	QRect rect = panel.frameGeometry();
-	rect.moveCenter( qa.desktop()->availableGeometry( &panel ).center() );
-	panel.move( rect.topLeft() );
+	QRect rect = gui.frameGeometry();
+	rect.moveCenter( qa.desktop()->availableGeometry( &gui ).center() );
+	gui.move( rect.topLeft() );
 	
 	// show game panel
-	panel.show();
-	*/
+	gui.show();
 	
 	/*
 	kBoard board;
 	board.show();
 	*/
-	
-	kGui gui;
-	gui.show();
 	
 	kXmppSettings settings("localhost", "guillaume", "localhost", "Home", "pouet");
 	KiessXmpp xmpp(settings);
