7418748fa3fa0ca60beff3c9e0628d30e513538b
pasnox <pasnox@a98a87ac-f22f-11dd-8c4e-4bbb75a408b7>
2009-02-23 21:42:42 +0000

    Add stuff to chat part

--- trunk/src/chat/kChatWidget.cpp
+++ trunk/src/chat/kChatWidget.cpp
@@ -18,10 +18,131 @@ kClient* kChatWidget::client() const
 	return mClient;
 }
 
-void kChatWidget::setClient( kClient* client )
+void kChatWidget::setClient( kClient* client, const QString& nick )
 {
 	if ( mClient != client )
 	{
-		qWarning() << "client setted";
+		if ( mClient )
+		{
+			disconnect( mClient, SIGNAL( incomingFileTransfer() ), this, SLOT( _q_incomingFileTransfer() ) );
+			disconnect( mClient, SIGNAL( rosterRequestFinished( bool ) ), this, SLOT( _q_rosterRequestFinished( bool ) ) );
+			disconnect( mClient, SIGNAL( newContact( const XMPP::RosterItem& ) ), this, SLOT( _q_newContact( const XMPP::RosterItem& ) ) );
+			disconnect( mClient, SIGNAL( contactDeleted( const XMPP::RosterItem& ) ), this, SLOT( _q_contactDeleted( const XMPP::RosterItem& ) ) );
+			disconnect( mClient, SIGNAL( contactUpdated( const XMPP::RosterItem& ) ), this, SLOT( _q_contactUpdated( const XMPP::RosterItem& ) ) );
+			disconnect( mClient, SIGNAL( resourceAvailable( const XMPP::Jid&, const XMPP::Resource& ) ), this, SLOT( _q_resourceAvailable( const XMPP::Jid&, const XMPP::Resource& ) ) );
+			disconnect( mClient, SIGNAL( resourceUnavailable( const XMPP::Jid&, const XMPP::Resource& ) ), this, SLOT( _q_resourceUnavailable( const XMPP::Jid&, const XMPP::Resource& ) ) );
+			disconnect( mClient, SIGNAL( messageReceived( const XMPP::Message& ) ), this, SLOT( _q_messageReceived( const XMPP::Message& ) ) );
+			disconnect( mClient, SIGNAL( groupChatJoined( const XMPP::Jid& ) ), this, SLOT( _q_groupChatJoined( const XMPP::Jid& ) ) );
+			disconnect( mClient, SIGNAL( groupChatLeft( const XMPP::Jid& ) ), this, SLOT( _q_groupChatLeft( const XMPP::Jid& ) ) );
+			disconnect( mClient, SIGNAL( groupChatPresence( const XMPP::Jid&, const XMPP::Status& ) ), this, SLOT( _q_groupChatPresence( const XMPP::Jid&, const XMPP::Status& ) ) );
+			disconnect( mClient, SIGNAL( groupChatError( const XMPP::Jid&, int, const QString& ) ), this, SLOT( _q_groupChatError( const XMPP::Jid&, int, const QString& ) ) );
+			disconnect( mClient, SIGNAL( subscription( const XMPP::Jid&, const QString& ) ), this, SLOT( _q_subscription( const XMPP::Jid&, const QString& ) ) );
+		}
+		
+		mClient = client;
+		
+		if ( mClient )
+		{
+			connect( mClient, SIGNAL( incomingFileTransfer() ), this, SLOT( _q_incomingFileTransfer() ) );
+			connect( mClient, SIGNAL( rosterRequestFinished( bool ) ), this, SLOT( _q_rosterRequestFinished( bool ) ) );
+			connect( mClient, SIGNAL( newContact( const XMPP::RosterItem& ) ), this, SLOT( _q_newContact( const XMPP::RosterItem& ) ) );
+			connect( mClient, SIGNAL( contactDeleted( const XMPP::RosterItem& ) ), this, SLOT( _q_contactDeleted( const XMPP::RosterItem& ) ) );
+			connect( mClient, SIGNAL( contactUpdated( const XMPP::RosterItem& ) ), this, SLOT( _q_contactUpdated( const XMPP::RosterItem& ) ) );
+			connect( mClient, SIGNAL( resourceAvailable( const XMPP::Jid&, const XMPP::Resource& ) ), this, SLOT( _q_resourceAvailable( const XMPP::Jid&, const XMPP::Resource& ) ) );
+			connect( mClient, SIGNAL( resourceUnavailable( const XMPP::Jid&, const XMPP::Resource& ) ), this, SLOT( _q_resourceUnavailable( const XMPP::Jid&, const XMPP::Resource& ) ) );
+			connect( mClient, SIGNAL( messageReceived( const XMPP::Message& ) ), this, SLOT( _q_messageReceived( const XMPP::Message& ) ) );
+			connect( mClient, SIGNAL( groupChatJoined( const XMPP::Jid& ) ), this, SLOT( _q_groupChatJoined( const XMPP::Jid& ) ) );
+			connect( mClient, SIGNAL( groupChatLeft( const XMPP::Jid& ) ), this, SLOT( _q_groupChatLeft( const XMPP::Jid& ) ) );
+			connect( mClient, SIGNAL( groupChatPresence( const XMPP::Jid&, const XMPP::Status& ) ), this, SLOT( _q_groupChatPresence( const XMPP::Jid&, const XMPP::Status& ) ) );
+			connect( mClient, SIGNAL( groupChatError( const XMPP::Jid&, int, const QString& ) ), this, SLOT( _q_groupChatError( const XMPP::Jid&, int, const QString& ) ) );
+			connect( mClient, SIGNAL( subscription( const XMPP::Jid&, const QString& ) ), this, SLOT( _q_subscription( const XMPP::Jid&, const QString& ) ) );
+			
+			Status status( Status::Online, tr( "Playing Kiess Game" ), 0 );
+			mClient->setPresence( status );
+			
+			mClient->joinGroupChat( "conference.jabber.org", "Kiess", nick );
+	
+			//requestRoster();
+		}
 	}
+}
+
+void kChatWidget::_q_incomingFileTransfer()
+{
+	qWarning() << "incomingFileTransfer";
+}
+
+void kChatWidget::_q_rosterRequestFinished( bool success )
+{
+	qWarning() << "rosterRequestFinished" << success;
+	/*
+	if ( success )
+	{
+		Message msg( Jid( "pasnox@gmail.com" ) );
+		msg.setFrom( jid() );
+		msg.setType( "chat" );
+		msg.setBody( "mon message depuis kiess", "fr" );
+		msg.addEvent( OfflineEvent );
+		msg.addEvent( ComposingEvent );
+		msg.addEvent( DeliveredEvent );
+		msg.addEvent( DisplayedEvent );
+		
+		sendMessage( msg );
+	}
+	*/
+}
+
+void kChatWidget::_q_newContact( const XMPP::RosterItem& item )
+{
+	qWarning() << "newContact";
+}
+
+void kChatWidget::_q_contactDeleted( const XMPP::RosterItem& item )
+{
+	qWarning() << "contactDeleted";
+}
+
+void kChatWidget::_q_contactUpdated( const XMPP::RosterItem& item )
+{
+	qWarning() << "contactUpdated";
+}
+
+void kChatWidget::_q_resourceAvailable( const XMPP::Jid& jid, const XMPP::Resource& resource )
+{
+	qWarning() << "resourceAvailable" << jid.full() << resource.name() << resource.status().show() << resource.status().status();
+}
+
+void kChatWidget::_q_resourceUnavailable( const XMPP::Jid& jid, const XMPP::Resource& resource )
+{
+	qWarning() << "resourceUnavailable" << jid.full() << resource.name() << resource.status().show() << resource.status().status();
+}
+
+void kChatWidget::_q_messageReceived( const XMPP::Message& message )
+{
+	qWarning() << "messageReceived";
+}
+
+void kChatWidget::_q_groupChatJoined( const XMPP::Jid& jid )
+{
+	qWarning() << "groupChatJoined" << jid.full();
+}
+
+void kChatWidget::_q_groupChatLeft( const XMPP::Jid& jid )
+{
+	qWarning() << "groupChatLeft" << jid.full();
+}
+
+void kChatWidget::_q_groupChatPresence( const XMPP::Jid& jid, const XMPP::Status& status )
+{
+	qWarning() << "groupChatPresence" << jid.full() << status.show() << status.status();
+}
+
+void kChatWidget::_q_groupChatError( const XMPP::Jid& jid, int error, const QString& reason )
+{
+	qWarning() << "groupChatError" << jid.full() << error << reason;
+}
+
+void kChatWidget::_q_subscription( const XMPP::Jid& jid, const QString& type )
+{
+	qWarning() << "subscription" << jid.full() << type;
 }
--- trunk/src/chat/kChatWidget.h
+++ trunk/src/chat/kChatWidget.h
@@ -13,7 +13,7 @@ public:
 	virtual ~kChatWidget();
 	
 	kClient* client() const;
-	void setClient( kClient* client );
+	void setClient( kClient* client, const QString& nick );
 
 public slots:
 
@@ -21,6 +21,19 @@ protected:
 	kClient* mClient;
 
 protected slots:
+	void _q_incomingFileTransfer();
+	void _q_rosterRequestFinished( bool success );
+	void _q_newContact( const XMPP::RosterItem& item );
+	void _q_contactDeleted( const XMPP::RosterItem& item );
+	void _q_contactUpdated( const XMPP::RosterItem& item );
+	void _q_resourceAvailable( const XMPP::Jid& jid, const XMPP::Resource& resource );
+	void _q_resourceUnavailable( const XMPP::Jid& jid, const XMPP::Resource& resource );
+	void _q_messageReceived( const XMPP::Message& message );
+	void _q_groupChatJoined( const XMPP::Jid& jid );
+	void _q_groupChatLeft( const XMPP::Jid& jid );
+	void _q_groupChatPresence( const XMPP::Jid& jid, const XMPP::Status& status );
+	void _q_groupChatError( const XMPP::Jid& jid, int error, const QString& reason );
+	void _q_subscription( const XMPP::Jid& jid, const QString& type );
 };
 
 #endif // KCHATWIDGET_H
